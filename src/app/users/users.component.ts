5f1e92193e0f5e7719da6cf11fa8a1ba427874b4
Michael D <michael@codetropolis.com>
2022-05-26 16:51:59 -0500

    Added Angular Material. Removed BrowserAnimationsModule from app.module as this BAM is imported in users standalone component.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatInputModule } from '@angular/material/input'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatInputModule, BrowserAnimationsModule],
   templateUrl: './users.component.html',
   styles: [
   ]
