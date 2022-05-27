96075c17c53a2f6bd5d32ab6342b6b8851ae8552
Michael D <michael@codetropolis.com>
2022-05-27 03:58:14 -0500

    Form type as UserForm inteface.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -5,6 +5,14 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatInputModule } from '@angular/material/input'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+export interface UserForm extends FormGroup<
+{
+  username: FormControl<string>,
+  firstName: FormControl<string>,
+  lastName: FormControl<string>,
+  age: FormControl<number>
+}>{} // Curly braces here?
+
 @Component({
   selector: 'app-users',
   standalone: true,
@@ -14,16 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ]
 })
 export class UsersComponent implements OnInit {
-  form!: FormGroup<{
-    username: FormControl<string>,
-    firstName: FormControl<string>,
-    lastName: FormControl<string>,
-    age: FormControl<number>,
-  }>;
+  // form!: FormGroup<{ 
+  //   username: FormControl<string>,
+  //   firstName: FormControl<string>,
+  //   lastName: FormControl<string>,
+  //   age: FormControl<number>,
+  // }>;
+  form!: UserForm;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.nonNullable.group({
-      username: this.fb.nonNullable.control('mdm115'),
+      username: this.fb.nonNullable.control('blackBriar'),
       firstName: this.fb.nonNullable.control('David'),
       lastName: this.fb.nonNullable.control('Webb'),
       age: this.fb.nonNullable.control(23),
@@ -38,7 +47,11 @@ export class UsersComponent implements OnInit {
       firstName: "Michael",
       lastName: "West",
       username: 'mWest997'
-    })
+    });
+  }
+
+  resetForm(){
+    this.form.reset();
   }
 
 }
